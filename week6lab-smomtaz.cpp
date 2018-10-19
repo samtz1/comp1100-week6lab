0e6d28c47ddf803b565b060621fd4e74fe9797b1
samtz1 <43217477+samtz1@users.noreply.github.com>
2018-10-18 23:47:14 -0400

    Add files via upload

---
+++ week6lab-smomtaz.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+int a = 50;
+a = grade;
+
+{
+cout<<"Enter score as number out of 100";
+if (a >= 86)
+cout<<"grade = A"<< endl;
+}
+
+    {
+    cout<<"Enter score as number out of 100";
+    if (a >= 72)
+    cout<<"grade = B"<< endl;
+    }
+
+        {
+        cout<<"Enter score as number out of 100";
+        if (a >= 60)
+        cout<<"grade = C"<< endl;
+        }
+
+            {
+            cout<<"Enter score as number out of 100";
+            if (a >= 50)
+            cout<<"grade = D"<< endl;
+            }
+
+                {
+                cout<<"Enter score as number out of 100";
+                if (a < 50)
+                cout<<"grade = F"<< endl;
+                }
+           
+
+return 0;
+}
